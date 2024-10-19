34a549ca52913aba7ccbd7c27928738b0ef43fb9
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-20 00:59:51 +0530

    oh well

--- prep/src/prep.cpp
+++ prep/src/prep.cpp
@@ -17,13 +17,13 @@ int     ifsatisfied[MAX_NESTED_IF_DEPTH] {};
 int     skipping {};
 
 int wmain(_In_opt_ int argc, _In_opt_count_(argc) wchar_t* argv[]) {
-    token_row tr;
-    long      t;
-    char      ebuf[BUFSIZ];
+    token_row    tr;
+    const time_t now { time(nullptr) };
+    char         ebuf[OUT_BUFF_SIZE];
 
     setbuf(stderr, ebuf);
-    t       = time(NULL);
-    curtime = ctime(t);
+
+    curtime = ctime(&now);
     maketokenrow(3, &tr);
     expandlex();
     setup(argc, argv);
@@ -48,7 +48,8 @@ void process(_In_ token_row* const tknrw) noexcept {
             anymacros             |= gettokens(tknrw, 1);
             tknrw->tp              = tknrw->bp;
         }
-        if (tknrw->tp->type == END) {
+
+        if (tknrw->tp->type == token_type::END) {
             if (--incdepth >= 0) {
                 if (cursource->ifdepth) error(ERROR, "Unterminated conditional in #include");
                 unsetsource();
@@ -60,11 +61,13 @@ void process(_In_ token_row* const tknrw) noexcept {
             if (ifdepth) error(ERROR, "Unterminated #if/#ifdef/#ifndef");
             break;
         }
+
         if (tknrw->tp->type == SHARP) {
             tknrw->tp += 1;
             control(tknrw);
         } else if (!skipping && anymacros)
             expandrow(tknrw, NULL, NOT_IN_MACRO);
+
         if (skipping) setempty(tknrw);
         puttokens(tknrw);
         anymacros        = 0;
@@ -73,27 +76,29 @@ void process(_In_ token_row* const tknrw) noexcept {
     }
 }
 
-void control(token_row* trp) noexcept {
+void control(token_row* tknrw) noexcept {
     Nlist* np {};
-    token* tp {};
+    token* tknptr {};
 
-    tp = trp->tp;
-    if (tp->type != NAME) {
-        if (tp->type == NUMBER) goto kline;
-        if (tp->type != NL) error(ERROR, "Unidentifiable control line");
+    tknptr = tknrw->tp;
+    if (tknptr->type != NAME) {
+        if (tknptr->type == NUMBER) goto kline;
+        if (tknptr->type != NL) error(ERROR, "Unidentifiable control line");
         return; /* else empty line */
     }
-    if ((np = lookup(tp, 0)) == NULL || (np->flag & ISKW) == 0 && !skipping) {
-        error(WARNING, "Unknown preprocessor control %t", tp);
+
+    if ((np = lookup(tknptr, 0)) == NULL || (np->flag & ISKW) == 0 && !skipping) {
+        error(WARNING, "Unknown preprocessor control %t", tknptr);
         return;
     }
+
     if (skipping) {
         if ((np->flag & ISKW) == 0) return;
         switch (np->val) {
             case KENDIF :
                 if (--ifdepth < skipping) skipping = 0;
                 --cursource->ifdepth;
-                setempty(trp);
+                setempty(tknrw);
                 return;
 
             case KIFDEF :
@@ -112,17 +117,17 @@ void control(token_row* trp) noexcept {
         }
     }
     switch (np->val) {
-        case KDEFINE : dodefine(trp); break;
+        case KDEFINE : dodefine(tknrw); break;
 
         case KUNDEF :
-            tp += 1;
-            if (tp->type != NAME || trp->lp - trp->bp != 4) {
+            tknptr += 1;
+            if (tknptr->type != NAME || tknrw->lp - tknrw->bp != 4) {
                 error(ERROR, "Syntax error in #undef");
                 break;
             }
-            if ((np = lookup(tp, 0))) {
+            if ((np = lookup(tknptr, 0))) {
                 if (np->flag & ISUNCHANGE) {
-                    error(ERROR, "#defined token %t can't be undefined", tp);
+                    error(ERROR, "#defined token %t can't be undefined", tknptr);
                     return;
                 }
                 np->flag &= ~ISDEFINED;
@@ -137,7 +142,7 @@ void control(token_row* trp) noexcept {
             if (++ifdepth >= MAX_NESTED_IF_DEPTH) error(FATAL, "#if too deeply nested");
             ++cursource->ifdepth;
             ifsatisfied[ifdepth] = 0;
-            if (eval(trp, np->val))
+            if (eval(tknrw, np->val))
                 ifsatisfied[ifdepth] = 1;
             else
                 skipping = ifdepth;
@@ -149,7 +154,7 @@ void control(token_row* trp) noexcept {
                 return;
             }
             if (ifsatisfied[ifdepth] == 2) error(ERROR, "#elif after #else");
-            if (eval(trp, np->val)) {
+            if (eval(tknrw, np->val)) {
                 if (ifsatisfied[ifdepth])
                     skipping = ifdepth;
                 else {
@@ -166,7 +171,7 @@ void control(token_row* trp) noexcept {
                 return;
             }
             if (ifsatisfied[ifdepth] == 2) error(ERROR, "#else after #else");
-            if (trp->lp - trp->bp != 3) error(ERROR, "Syntax error in #else");
+            if (tknrw->lp - tknrw->bp != 3) error(ERROR, "Syntax error in #else");
             skipping             = ifsatisfied[ifdepth] ? ifdepth : 0;
             ifsatisfied[ifdepth] = 2;
             break;
@@ -178,46 +183,46 @@ void control(token_row* trp) noexcept {
             }
             --ifdepth;
             --cursource->ifdepth;
-            if (trp->lp - trp->bp != 3) error(WARNING, "Syntax error in #endif");
+            if (tknrw->lp - tknrw->bp != 3) error(WARNING, "Syntax error in #endif");
             break;
 
         case KERROR :
-            trp->tp = tp + 1;
-            error(ERROR, "#error directive: %r", trp);
+            tknrw->tp = tknptr + 1;
+            error(ERROR, "#error directive: %r", tknrw);
             break;
 
         case KWARNING :
-            trp->tp = tp + 1;
-            error(WARNING, "#warning directive: %r", trp);
+            tknrw->tp = tknptr + 1;
+            error(WARNING, "#warning directive: %r", tknrw);
             break;
 
         case KLINE :
-            trp->tp = tp + 1;
-            expandrow(trp, "<line>", NOT_IN_MACRO);
-            tp = trp->bp + 2;
+            tknrw->tp = tknptr + 1;
+            expandrow(tknrw, "<line>", NOT_IN_MACRO);
+            tknptr = tknrw->bp + 2;
 kline:
-            if (tp + 1 >= trp->lp || tp->type != NUMBER || tp + 3 < trp->lp ||
-                (tp + 3 == trp->lp && ((tp + 1)->type != STRING) || *(tp + 1)->t == 'L')) {
+            if (tknptr + 1 >= tknrw->lp || tknptr->type != NUMBER || tknptr + 3 < tknrw->lp ||
+                (tknptr + 3 == tknrw->lp && ((tknptr + 1)->type != STRING) || *(tknptr + 1)->t == 'L')) {
                 error(ERROR, "Syntax error in #line");
                 return;
             }
-            cursource->line = atol((char*) tp->t) - 1;
+            cursource->line = atol((char*) tknptr->t) - 1;
             if (cursource->line < 0 || cursource->line >= 32768) error(WARNING, "#line specifies number out of range");
-            tp = tp + 1;
-            if (tp + 1 < trp->lp) cursource->filename = (char*) newstring(tp->t + 1, tp->len - 2, 0);
+            tknptr = tknptr + 1;
+            if (tknptr + 1 < tknrw->lp) cursource->filename = (char*) newstring(tknptr->t + 1, tknptr->len - 2, 0);
             return;
 
         case KDEFINED : error(ERROR, "Bad syntax for control line"); break;
 
         case KINCLUDE :
-            doinclude(trp);
-            trp->lp = trp->bp;
+            doinclude(tknrw);
+            tknrw->lp = tknrw->bp;
             return;
 
-        case KEVAL : eval(trp, np->val); break;
+        case KEVAL : eval(tknrw, np->val); break;
 
-        default    : error(ERROR, "Preprocessor control `%t' not yet implemented", tp); break;
+        default    : error(ERROR, "Preprocessor control `%t' not yet implemented", tknptr); break;
     }
-    setempty(trp);
+    setempty(tknrw);
     return;
 }
